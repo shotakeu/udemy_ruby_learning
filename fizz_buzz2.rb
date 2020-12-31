4f7e6ea536f78df2bf7e79c5c2a16b37f8a6a52b
Shotaro Takeuchi <taro20090819@yahoo.co.jp>
2020-12-31 11:48:38 +0900

    add fizz_buzz2.rb

---
+++ fizz_buzz2.rb
@@ -0,0 +1,20 @@
+def fizz_buzz
+  for num in 1 .. 100 do
+
+      if num % 3 == 0 && num % 5 == 0
+          puts "FizzBuzz"
+
+      elsif num % 3 == 0
+          puts "Fizz"
+
+      elsif num % 5 == 0
+          puts "Buzz"
+
+      else 
+          puts num
+      end
+
+  end
+end
+
+fizz_buzz
